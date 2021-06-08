db40bcee1b24ca53e58b98868e73b76ffb4095b7
unknown <aaarevalo@crimson.ua.edu>
2021-06-08 07:32:21 -0500

    vec4buffer

--- src/main/java/org/artifex/Program.java
+++ src/main/java/org/artifex/Program.java
@@ -1,6 +1,7 @@
 package org.artifex;
 
 import org.artifex.math.Vector3fBuffer;
+import org.artifex.math.Vector4fBuffer;
 import org.artifex.props.AppProperties;
 import org.artifex.util.Pointers;
 import org.artifex.util.SPIRV;
@@ -10,6 +11,7 @@ import org.artifex.vulkan.compute.DescriptorBindings;
 import org.artifex.vulkan.compute.DescriptorCopyWrite;
 import org.artifex.vulkan.compute.DescriptorSet;
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.lwjgl.system.MemoryUtil;
 
 import java.nio.FloatBuffer;
@@ -30,7 +32,7 @@ public class Program
         device = new Device(physicalDevice);
 
         DescriptorBindings bindings = new DescriptorBindings(1,VK_SHADER_STAGE_COMPUTE_BIT)
-                .add(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,0,1,Vector3fBuffer.SIZEOF*256);
+                .add(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,0,1, Vector4fBuffer.SIZEOF*256);
 //                .add(VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,1,1,Integer.BYTES*256);
 
         computeQueue = new Queue.ComputeQueue(device,0);
@@ -48,18 +50,13 @@ public class Program
 
          signal = new Semaphore(device);
 
-        System.out.println("---------------------------------------------------------");
 
 
         long map1 = set.getBuffer(0).map();
-//        long map2 = set.getBuffer(1).map();
-        Vector3fBuffer indat = Vector3fBuffer.memVec3fBuffer(map1,256);
-//        FloatBuffer indat2 = MemoryUtil.memFloatBuffer(map2,256);
+        Vector4fBuffer indat = Vector4fBuffer.memVector4fBuffer(map1,256);
         for (int i = 0; i < indat.capacity(); i++) {
-            indat.setX(i,100);
-//            indat2.put(i,10);
+            indat.putX(i,100);
         }
-//        set.getBuffer(1).unmap();
         set.getBuffer(0).unmap();
 
         copyWrite.updateDescriptorSets();
@@ -68,7 +65,6 @@ public class Program
         record(set,bindings,compute);
 
 
-//        try(MemoryStack stack = MemoryStack.stackPush()){
             IntBuffer shstg = MemoryUtil.memAllocInt(1).put(0,VK_SHADER_STAGE_COMPUTE_BIT);
 
 
@@ -81,24 +77,14 @@ public class Program
         computeQueue.waitIdle();
         device.waitIdle();
         fence.fenceWait();
-//        copyWrite.updateDescriptorSets();
-//        vkMapMemory(device.getDevice(),set.getBuffer(1).getMemory(),0,Integer.BYTES*64,0, pLoad);
-        Vector3fBuffer ibuff = Vector3fBuffer.memVec3fBuffer(set.getBuffer(0).map(),256);
+        Vector4fBuffer ibuff = Vector4fBuffer.memVector4fBuffer(set.getBuffer(0).map(),256);
 
-//        FloatBuffer output =MemoryUtil.memFloatBuffer(set.getBuffer(1).map(),256);// MemoryUtil.memIntBuffer(set.getBuffer(1).map(),100);
-        Vector3f[] vecs = ibuff.getVectors();
+        Vector4f[] vecs = ibuff.toVector4f();
         FloatBuffer sasdf = ibuff.getBuffer();
         for (int i = 0; i < ibuff.capacity(); i++) {
             System.out.println(vecs[i]);
-//            System.out.println(output.get(i));
         }
         set.getBuffer(0).unmap();
-//        set.getBuffer(1).unmap();
-
-//        while(w.alive()){
-//            w.pollEvents();
-//        }
-
 
     }
 
--- src/main/java/org/artifex/math/Vector4fBuffer.java
+++ src/main/java/org/artifex/math/Vector4fBuffer.java
@@ -14,56 +14,111 @@ public class Vector4fBuffer
     }
 
 
+    private Vector4fBuffer(int capacity){
+        this.buffer=MemoryUtil.memAllocFloat(capacity*4);
+    }
+
     public static Vector4fBuffer memVector4fBuffer(long address, int capacity){
         Vector4fBuffer buff = new Vector4fBuffer(address,capacity);
 
         return  buff;
     }
-
-
-    public void put3(float[] xyz){
-
+    public static Vector4fBuffer memAllocVector4f(int capacity){
+        return new Vector4fBuffer(capacity);
     }
 
-    public void put(float[] xyzw){
-
+    public float x(int index){
+        return buffer.get(index*4);
     }
+    public float y(int index){
+        return buffer.get(index*4+1);
+    }
+    public float z(int index){
+        return buffer.get(index*4+2);
+    }
+    public float w(int index){
+        return buffer.get(index*4+3);
+    }
+
+    public void putX(int index,float x){
+        buffer.put(index*4,x);
+    }
+    public void putY(int index,float y){
+        buffer.put(index*4+1,y);
+    }
+    public void putZ(int index,float z){
+        buffer.put(index*4+2,z);
+    }
+    public void putW(int index,float w){
+        buffer.put(index*4+3,w);
+    }
+
+
 
     public void put3(int index, float[] xyz){
-
+        putX(index,xyz[0]);
+        putY(index,xyz[1]);
+        putZ(index,xyz[2]);
     }
 
     public void put(int index, float[] xyzw){
-
+        putX(index,xyzw[0]);
+        putY(index,xyzw[1]);
+        putZ(index,xyzw[2]);
+        putW(index,xyzw[3]);
     }
 
-    public void put(Vector3f vector3f){
-
+    public void put(int index,Vector3f vector3f){
+        putX(index,vector3f.x);
+        putY(index,vector3f.y);
+        putZ(index,vector3f.z);
     }
 
-    public void put(Vector4f vector4f){
-
+    public void put(int index,Vector4f vector4f){
+        putX(index,vector4f.x);
+        putY(index,vector4f.y);
+        putZ(index,vector4f.z);
+        putW(index,vector4f.w);
     }
 
     public Vector3f get3f(int i){
-        return null;
+        return new Vector3f(x(i),y(i),z(i));
     }
 
-    public Vector4f get(int index){
-        return null;
+    public Vector4f get(int i){
+        return new Vector4f(x(i),y(i),z(i),w(i));
     }
 
     public Vector3f[] toVector3f(){
-        return null;
+        Vector3f[] vector3fs = new Vector3f[capacity()];
+        for (int i = 0; i < vector3fs.length; i++) {
+            vector3fs[i]=get3f(i);
+        }
+        return vector3fs;
     }
 
     public Vector4f[] toVector4f(){
-        return null;
+        Vector4f[] vector4fs = new Vector4f[capacity()];
+        for (int i = 0; i < vector4fs.length; i++) {
+            vector4fs[i]=get(i);
+        }
+        return vector4fs;
     }
 
     public void free(){
         buffer.clear();
     }
+
+    public int capacity(){
+        return buffer.capacity()/4;
+    }
+
+    public FloatBuffer getBuffer() {
+        return buffer;
+    }
+
     private final FloatBuffer buffer;
+
+    public static final long SIZEOF= Float.BYTES*4;
 
 }
